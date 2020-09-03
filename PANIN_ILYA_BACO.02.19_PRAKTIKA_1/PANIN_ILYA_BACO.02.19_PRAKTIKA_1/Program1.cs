70e4f9a00984fb3d9641fff91c76bdfb51b2dc6a
PaNuSHa <70700507+panusha-official@users.noreply.github.com>
2020-09-03 17:37:47 +0300

    Add files via upload

---
+++ PANIN_ILYA_BACO.02.19_PRAKTIKA_1/PANIN_ILYA_BACO.02.19_PRAKTIKA_1/Program1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PANIN_ILYA_BACO._02._19_PRAKTIKA_1
+{
+  class Program
+  {
+    struct Person
+    {
+      public enum Genders : int { Male, Female };
+      public string firstName;
+      public string lastName;
+      public int age;
+      public Genders gender;
+
+      public Person(string _firstName, string _lastName, int _age, Genders _gender)
+      {
+        firstName = _firstName; lastName =
+        _lastName; age = _age; gender = _gender;
+      }
+
+      public override string ToString()
+      {
+        return firstName + " " + lastName + " (" + gender + "), age " + age;
+      }
+    }
+    static void Main(string[] args)
+    {
+      Person p = new Person("Tony", "Allen", 32, Person.Genders.Male);
+      Console.WriteLine(p.ToString());
+    }
+  }
+}
---
+++ PANIN_ILYA_BACO.02.19_PRAKTIKA_2/PANIN_ILYA_BACO.02.19_PRAKTIKA_2/Program2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PANIN_ILYA_BACO._02._19_PRAKTIKA_2
+{
+  class Program
+  {
+    
+    static void Main(string[] args)
+    {
+      SByte a = 0;
+      Byte b = 0;
+      Int16 c = 0;
+      Int32 d = 0;
+      Int64 e = 0;
+      string s = "";
+      Exception ex = new Exception();
+
+      object[] types = { a, b, c, d, e, s, ex };
+
+      foreach (object o in types)
+      {
+        string type;
+        if (o.GetType().IsValueType) type = "Value type";
+        else
+          type = "Reference Type";
+        Console.WriteLine("{0}: {1}", o.GetType(), type);
+      }
+    }
+  }
+}
